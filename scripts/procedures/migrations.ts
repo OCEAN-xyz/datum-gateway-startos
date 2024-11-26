994ff66df4a5b9c7bbb1b5f653c18a30b9a18b06
Shadowy Super Coder <musashidisciple@proton.me>
2024-11-25 22:16:33 -0700

    fix migration

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,54 @@
-import { compat, types as T } from "../deps.ts";
+import { compat, matches, types as T } from "../deps.ts";
 
-export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping({}, "0.3.5" );
+export const migration: T.ExpectedExports.migration =
+  compat.migrations.fromMapping(
+    {
+      "0.2.1": {
+        up: compat.migrations.updateConfig(
+          (config) => {
+            if (
+              matches
+                .shape({
+                  logger: matches.shape({
+                    log_to_file: matches.any,
+                    log_file: matches.any,
+                    log_level_file: matches.any,
+                  }),
+                })
+                .test(config)
+            ) {
+              config.logger.log_to_file = false;
+              config.logger.log_file = "/root/start9/logs.txt";
+              config.logger.log_level_file = 1;
+            }
+            return config;
+          },
+          false,
+          { version: "0.2.1", type: "up" }
+        ),
+        down: compat.migrations.updateConfig(
+          (config) => {
+            if (
+              matches
+                .shape({
+                  logger: matches.shape({
+                    log_to_file: matches.any,
+                    log_file: matches.any,
+                    log_level_file: matches.any,
+                  }),
+                })
+                .test(config)
+            ) {
+              delete config.logger.log_to_file;
+              delete config.logger.log_file;
+              delete config.logger.log_level_file;
+            }
+            return config;
+          },
+          true,
+          { version: "0.2.1", type: "down" }
+        ),
+      },
+    },
+    "0.2.1"
+  );
