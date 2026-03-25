5246a74c37ebfde130f8b248a8874a11ff9146d2
Matt Hill <mattnine@protonmail.com>
2026-03-24 18:10:45 -0600

    sdk beta.66 + version bump

--- startos/index.ts
+++ startos/index.ts
@@ -7,5 +7,5 @@ export { init, uninit } from './init'
 export { main } from './main'
 import { buildManifest } from '@start9labs/start-sdk'
 import { manifest as sdkManifest } from './manifest'
-import { versionGraph } from './versions/versionGraph'
+import { versionGraph } from './versions'
 export const manifest = buildManifest(versionGraph, sdkManifest)
--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_0_4_1_2_b6 } from './v0.4.1.2.b6'
+import { v_0_4_1_2_b7 } from './v0.4.1.2.b7'
 
 export const versionGraph = VersionGraph.of({
-  current: v_0_4_1_2_b6,
+  current: v_0_4_1_2_b7,
   other: [],
 })
